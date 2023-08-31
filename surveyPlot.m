fcbedac3ed99cb6f82a3b97e6f02a34523d092e1
pedgarcia1 <pedgarcia@itba.edu.ar>
2023-08-31 15:02:37 -0300

    surveyPlot terminado, distancas calculadas

--- surveyPlot.m
+++ surveyPlot.m
@@ -10,7 +10,7 @@ meanStage1 = [0 0 0];
 clusters{1} = importClustersNew("ET2022h clusters.csv");
 clusters{2} = importClustersNew("ET2024h clusters.csv");
 
-figure; hold on; 
+figure; hold on;
 f1 = gca();
 xlabel(f1,'x [m]'); ylabel(f1,'y [m]'); zlabel(f1,'depth [m]');
 set(f1,'ZDir','reverse');
@@ -20,13 +20,13 @@ for i = 1:nCasings
     TVD = casing{i}.TVDm(~isnan(casing{i}.TVDm));
     plot3(f1,EW+locationGrid(1)-meanStage1(1),NS+locationGrid(2)-meanStage1(2),TVD,'o')
 end
-
+hold on
 c = 1;
 for i = 1:size(clusters,2)
     EW = casing{i}.EWm(~isnan(casing{i}.EWm));
     NS = casing{i}.NSm(~isnan(casing{i}.NSm));
     TVD = casing{i}.TVDm(~isnan(casing{i}.TVDm));
-    MD = casing{i}.MDm(~isnan(casing{i}.MDm));
+    MD = casing{i}.MDm(~isnan(casing{i}.TVDm));
     for n = 0:2
         cTVD = mean(clusters{1},2);
         meanTVD = mean(cTVD(10*n+1:10*(n+1)));
@@ -34,4 +34,14 @@ for i = 1:size(clusters,2)
         casingPoints(c,:) = [EW(I) NS(I) TVD(I)];
         c = c + 1;
     end
+end
+plot3(f1,casingPoints(:,1)+locationGrid(1)-meanStage1(1),casingPoints(:,2)+locationGrid(2)-meanStage1(2),casingPoints(:,3),'+','Color',[0 0 0])
+for i = 1:size(casingPoints,1)
+    text(casingPoints(i,1)+locationGrid(1)-meanStage1(1), casingPoints(i,2)+locationGrid(2)-meanStage1(2), casingPoints(i,3)+200, num2str(i), 'FontSize', 10);
+end
+for i = [1 2 4 5]
+    resultados.dStages(i) = norm(casingPoints(i,:)-casingPoints(i+1,:));
+end
+for i = 1:size(casingPoints,1)/2
+    resultados.dCasings(i) = norm(casingPoints(i,:)-casingPoints(i+3,:));
 end
